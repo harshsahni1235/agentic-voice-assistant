fa0f951e20bca968e0a29d0ef4f723973213966d
Harsh <harsh.sahni@aaveg.com>
2026-01-29 00:42:10 +0530

    test: CD pipeline

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import express from "express";
+import readline from "readline";
 import { orchestrator } from "./core/orchestrator";
+import express from "express";
 
 const app = express();
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "ok" });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+
+// async function main() {
+//   const result = await orchestrator.run({
+//     input: "Read this explanation aloud",
+//   });
+
+//   console.log("Final Result:", result);
+//   console.log("Final Output:", result.output);
+// }
+
+// main();
 
 app.post("/run", async (req, res) => {
   const { input } = req.body;
@@ -18,3 +32,16 @@ app.post("/run", async (req, res) => {
 app.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000");
 });
+
+async function ask() {
+  rl.question("> ", async (userInput) => {
+    const result = await orchestrator.run({ input: userInput });
+    console.log("question", result.output);
+
+    ask(); // 🔁 keep listening
+  });
+}
+
+console.log("Agentic Voice Assistant started. Type your message:");
+ask();
+
